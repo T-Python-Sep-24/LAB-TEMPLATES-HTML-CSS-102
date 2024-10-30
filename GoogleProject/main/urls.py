b7aa62c7666da2a7ba5a65c3a42d9adaf739986c
KhuloodGhazi <khulood.gmo@gmail.com>
2024-10-30 19:35:36 +0300

    Finish the first page

--- GoogleProject/main/urls.py
+++ GoogleProject/main/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.google_view, name="google"),
-    path("google/", views.google_view, name="google"),
+    path("", views.main_view, name="main_view"),
+    path("google/", views.main_view, name="google"),
     path("erms and services/", views.term_view, name="term"),
 
 
