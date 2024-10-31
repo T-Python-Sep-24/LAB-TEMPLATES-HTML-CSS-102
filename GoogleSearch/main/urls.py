6653e3771019083872dc480f6e2b1d63ea3560d5
Ali <almaghamsiali@gmail.com>
2024-10-31 14:04:48 +0300

    added a veiw mode for ther user

--- GoogleSearch/main/urls.py
+++ GoogleSearch/main/urls.py
@@ -6,5 +6,8 @@ app_name='main'
 
 urlpatterns=[
     path('',views.google_view,name='google_view'),
-    path('Terms/of/Service/',views.tos_view,name='tos_view')
+    path('Terms/of/Service/',views.tos_view,name='tos_view'),
+    path('night/mode/',views.night_mode_view,name='night_mode_view'),
+    path('light/mode/',views.light_mode_view,name='light_mode_view'),
+
 ]
--- GoogleSearch/main/views.py
+++ GoogleSearch/main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.shortcuts import render,redirect
+from django.http import HttpRequest,HttpResponse
 
 
 def google_view(request:HttpRequest):
@@ -7,3 +7,16 @@ def google_view(request:HttpRequest):
 
 def tos_view(request:HttpRequest):
     return render(request,'main/tos.html')
+
+def night_mode_view(request:HttpRequest):
+
+    response=redirect('main:google_view')
+    response.set_cookie('darkmode','dark',max_age=60*60*24)
+    return response
+
+def light_mode_view(request:HttpRequest):
+    response=redirect('main:google_view')
+    response.set_cookie('darkmode','dark',max_age=-3600)
+    return response
+
+    
